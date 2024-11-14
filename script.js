23d05793a124eeccd5333009c16b4713b0b81e57
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-11-14 23:26:17 +0600

    Added functionality for set data in localStorage

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box")
 
+//set data in localstorage
+function updateStorage(){
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+//creating note
 createBtn.addEventListener("click", ()=> {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -9,5 +16,21 @@ createBtn.addEventListener("click", ()=> {
   inputBox.setAttribute("contenteditable", "true");
   img.src = "images/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
-  
+
+})
+
+//delete note
+notesContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "IMG"){
+    e.target.parentElement.remove();
+    updateStorage();
+  }
+  else if(e.target.tagName === "p"){
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach(nt => {
+      nt.onkeyup = function(){
+        updateStorage();
+      }
+    })
+  }
 })
